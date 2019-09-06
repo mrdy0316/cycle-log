1278dc5d4c7923e8f49098dc5617a14326fab6ab
mrdy0316 <mt.maru.dai0316@gmail.com>
2019-09-06 12:11:13 +0900

    validation

--- html/index.php
+++ html/index.php
@@ -3,38 +3,46 @@
 
 </head>
 <body>
-<div id='btn'>button!!!</div>
-
+<div id='btn'></div>
 <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script>
 window.onload = function() {
-    if(navigator.geolocation) {//whether use Geolocation API
-        function postData(pos) {
-            $.ajax({
-                url: './gps_insert.php',
-                type: 'POST',
-                dataType: 'json',
-                data : {
-                    latitude: pos.coords.latitude,
-                    longitude: pos.coords.longitude
+    var btnTxtOff = 'START';
+    var btnTxtOn = 'STOP';
+    var btnFlag = false;//off
+    var btn = $('#btn');
+    btn.text(btnTxtOff);
+    btn.on('click', function() {
+        if(btnFlag == false) {
+            if(navigator.geolocation) {//whether user can use Geolocation API
+                btnFlag = true;
+                btn.text(btnTxtOn);
+                function postData(pos) {
+                    $.ajax({
+                        url: './gps_insert.php',
+                        type: 'POST',
+                        dataType: 'json',
+                        data : {
+                            latitude: pos.coords.latitude,
+                            longitude: pos.coords.longitude
+                        }
+                    })
                 }
-            })
-            console.log('submit');
-        }
-        var option = {
-            'enableHighAccuracy': false,
-            'maximumAge': 10000,
-        } ;
-        id = navigator.geolocation.watchPosition(postData,null,option);
-        $('#btn').on('click', function() {
+                var option = {
+                    'enableHighAccuracy': false,
+                    'maximumAge': 10000,
+                } ;
+                id = navigator.geolocation.watchPosition(postData,null,option);
+            } else {
+                alert('Geolocation API対応ブラウザでアクセスしてください。');
+            }
+        } else {
+            btnFlag = false;
+            btn.text(btnTxtOff);
             navigator.geolocation.clearWatch(id);
-            console.log('fin.');
-        });
-    } else {
-        alert('Geolocation API対応ブラウザでアクセスしてください。');
-    }
+        }
+    })
 }
-
 </script>
 </body>
 </html>
